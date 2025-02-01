c38d3b1c0078bdf5f691b5458bdb301e7049d50d
Shubham <shubhamkarampure26@gmail.com>
2025-02-01 13:13:48 +0530

    added learn

---
+++ src/app/learn/sorting/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import EditorComponent from '@/components/ui/EditorComponent';
+const SortingPage: React.FC = () => {
+    return (
+        <div>
+            <EditorComponent/>
+            <h1>Sorting Algorithms</h1>
+            <p>Welcome to the sorting algorithms learning page!</p>
+        </div>
+    );
+};
+
+export default SortingPage;
